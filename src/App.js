2f2909a031abbedfa510f021e4b27a9b242a31f4
Erik Huang <ekhu94@gmail.com>
2021-04-28 22:20:52 -0700

    lists index of food below form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
 const App = () => {
+  const [foods, setFoods] = useState([]);
   const [foodName, setFoodName] = useState("");
   const [daysSince, setDaysSince] = useState(0);
 
+  useEffect(() => {
+    const getFoods = async () => {
+      const res = await axios.get("http://localhost:5000/foods");
+      setFoods(res.data);
+    };
+    getFoods();
+  }, []);
+
   const addToList = async (e) => {
-    await axios.post("http://localhost:5000/foods", {
+    const res = await axios.post("http://localhost:5000/foods", {
       name: foodName,
       daysSinceEaten: daysSince,
     });
+    setFoods([...foods, res.data]);
     setFoodName("");
     setDaysSince(0);
+  };
+
+  const renderFoods = () => {
+    return foods.map((food) => {
+      return (
+        <h4 key={food.id}>
+          {food.name} - {food.daysSinceEaten}{" "}
+          {food.daysSinceEaten === 1 ? "day" : "days"} ago
+        </h4>
+      );
+    });
   };
 
   return (
@@ -34,6 +55,9 @@ const App = () => {
         onChange={(e) => setDaysSince(e.target.value)}
       />
       <button onClick={addToList}>Add Food</button>
+
+      <h2>Food List</h2>
+      {renderFoods()}
     </div>
   );
 };
